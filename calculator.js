ea3c8dc4b88ef075c022160be72d633c63923bf5
chefkasperson <chefkasperson@gmail.com>
2020-07-16 06:33:14 -0700

    added bmi calculator

--- calculator.js
+++ calculator.js
@@ -12,9 +12,20 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html')
 })
 
+app.get('/bmiCalculator.html', function(req, res) {
+    res.sendFile(__dirname + '/bmiCalculator.html')
+})
+
 app.post('/index.html', function(req, res) {
     let num1 = Number(req.body.num1)
     let num2 = Number(req.body.num2)
     let result = num1 + num2
     res.send('The calculation result is ' + result + '!')
+})
+
+app.post('/bmiCalculator.html', function(req, res) {
+    let height = parseFloat(req.body.height)
+    let weight = parseFloat(req.body.weight)
+    let result = ( weight ) / ((height/100)**2)
+    res.send('Your BMI is ' + result + '!')
 })
