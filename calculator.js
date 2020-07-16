5c01d4d9ca0a7cb300ab11587ace5460eee65305
chefkasperson <chefkasperson@gmail.com>
2020-07-16 05:04:28 -0700

    added post route

--- calculator.js
+++ calculator.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(3000)
+app.listen(3000, function(){
+    console.log('server is running on port 3000')
+})
 
 app.get('/', function(req, res) {
-    res.send('Hello World')
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/index.html', function(req, res) {
+    let num1 = Number(req.body.num1)
+    let num2 = Number(req.body.num2)
+    let result = num1 + num2
+    res.send('The calculation result is ' + result + '!')
 })
